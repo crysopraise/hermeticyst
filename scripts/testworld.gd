fec1d5e5f46842f6e70d543a21392f3523bf5d4b
Rhea Willow S <rheawillows@MILF-machine.localdomain>
2021-09-06 12:13:28 -0700

    High poly test

---
+++ scripts/testworld.gd
@@ -0,0 +1,15 @@
+extends Spatial
+
+var guy_count = 1
+
+func _process(delta):
+	$FPS.text = str(Engine.get_frames_per_second()) + " fps"
+
+	if Input.is_action_just_pressed("debug_button"):
+		var guy = preload("res://scenes/enemies/enemy.tscn").instance()
+		guy.translation = $Player.translation
+		guy.translation.z -= 3
+		add_child(guy)
+		guy_count += 1
+		$GuyCount.text = "Guys: " + str(guy_count)
+
