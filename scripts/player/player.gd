fc1590cd9900cb5cdb2b8945438b00f08668d6b5
Rhea Willow S <rheawillows@MILF-machine.localdomain>
2021-09-09 15:23:06 -0700

    Added test animation and level
    - Added a test aniation for player attack
    - Added collision for attack animation
    - Added a test level mesh lulu made
    - Tweaked movement values, removed speeding up from blood cysts
    - Added a small boost while attacking
    - Put camera on a pivot

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,33 +1,36 @@
 extends KinematicBody
 
-export var MAX_VELOCITY = 50
-export var ACCELERATION = 0.025
-export var DRAG = 0.025
+export var MAX_VELOCITY = 25
+export var ACCELERATION = 0.015
+export var DRAG = 0.015
+export var ATTACK_VELOCITY = 15
 export var MOUSE_SENSITIVITY = 0.1
 export var MAX_VERTICLE_LOOK_ANGLE = 30
-export var BASE_LOOK_ANGLE = -25
+export var BASE_LOOK_ANGLE = 0
 export var STRAFE_TILT_ANGLE = 10
 export var CAMERA_LAG = 0.01
 export var CAMERA_LAG_RATIOS = Vector3(1.5, 0.5, 1)
 
 # Blood constants
-export var BASE_BLOOD_TOTAL = 50
-export var BASE_BLOOD_REGEN = 5 # per second
+export var BASE_BLOOD_TOTAL = 100
+export var BASE_BLOOD_REGEN = 1 # per second
 export var BOOST_BLOOD_COST = 20 # per second
-export var BASE_BOOST_SPEED_MODIFIER = 1.5
+export var ATTACK_BLOOD_COST = 15
+export var BASE_BOOST_SPEED_MODIFIER = 1.75
 #export var BASE_BOOST_MAX_VEL_MODIFIER = 1.5
 
 # Blood value constants
-export var TOTAL_RATIO = 10
-export var REGEN_RATIO = 1
-export var SPEED_RATIO = 0.2
+export var TOTAL_INCREASE_RATIO = 10
+export var REGEN_INCREASE_RATIO = 1
+export var SPEED_INCREASE_RATIO = 0
 
 onready var player_attack_scn = preload("res://scenes/player/player_attack.tscn")
 
-onready var camera = get_node('Camera')
-onready var mesh = $Mesh
+onready var camera = $Pivot
+onready var model = $Model
 onready var blood_remaining_bar = $HealthBar/Remaining
 onready var blood_total_bar = $HealthBar/Total
+onready var animation = $Model/AnimationPlayer
 
 var velocity = Vector3.ZERO
 var blood = BASE_BLOOD_TOTAL
@@ -36,6 +39,7 @@ var blood_regen_modifier = 0
 var blood_speed_modifier = 1
 var boost_speed_modifier = BASE_BOOST_SPEED_MODIFIER
 #var boost_vel_modifier = BASE_BOOST_MAX_VEL_MODIFIER
+var is_attacking = false
 var camera_offset
 var player_attack
 
@@ -55,9 +59,11 @@ func _input(event):
 			deg2rad(BASE_LOOK_ANGLE - MAX_VERTICLE_LOOK_ANGLE),
 			deg2rad(BASE_LOOK_ANGLE + MAX_VERTICLE_LOOK_ANGLE))
 		rotation.y += -deg2rad(movement.x * MOUSE_SENSITIVITY)
+		model.rotation.x = camera.rotation.x
 
 func _physics_process(delta):
 	var input_velocity = Vector3.ZERO
+	var attack_velocity = Vector3.ZERO
 	var is_boosting = false
 
 	if Input.is_action_pressed('left'):
@@ -72,13 +78,10 @@ func _physics_process(delta):
 		input_velocity.y -= 1
 	if Input.is_action_pressed('up'):
 		input_velocity.y += 1
-	if Input.is_action_pressed('attack') and !player_attack:
-		print('attacked')
-		player_attack = preload("res://scenes/player/player_attack.tscn").instance()
-		player_attack.connect("body_entered", self, "_on_entity_hit")
-		add_child(player_attack)
-		player_attack.translation.z -= 3
-		$AttackTimer.start()
+	if Input.is_action_pressed('attack') and !is_attacking:
+		blood -= ATTACK_BLOOD_COST
+		animation.play("Attack", 0.5)
+		is_attacking = true
 	if Input.is_action_pressed('boost') and blood > 0:
 		is_boosting = true
 		blood -= BOOST_BLOOD_COST * delta
@@ -101,10 +104,10 @@ func _physics_process(delta):
 		velocity = velocity.linear_interpolate(input_velocity, accelaration)
 	else:
 		velocity = velocity.linear_interpolate(Vector3.ZERO, DRAG)
-	
+
 	var rotated_velocity = velocity.rotated(Vector3.UP, -rotation.y)
 
-	mesh.rotation.z = -rotated_velocity.x \
+	model.rotation.z = -rotated_velocity.x \
 			/ MAX_VELOCITY \
 			* deg2rad(STRAFE_TILT_ANGLE)
 
@@ -130,17 +133,26 @@ func _on_attack_timeout():
 func _on_entity_hit(body):
 	if body.is_in_group("enemies"):
 		print("enemy hit")
-		body.damage(velocity)
+		print(body.translation, translation)
+		var hit_velocity = (body.translation - translation).normalized() * 10
+		body.damage(hit_velocity)
 	if body.is_in_group("cysts"):
 		_increase_blood(body.blood_value)
 		body.queue_free()
 
 func _increase_blood(blood_value):
-	blood_total_modifier += blood_value * TOTAL_RATIO
-	blood_regen_modifier += blood_value * REGEN_RATIO
-	boost_speed_modifier += blood_value * SPEED_RATIO
+	blood_total_modifier += blood_value * TOTAL_INCREASE_RATIO
+	blood_regen_modifier += blood_value * REGEN_INCREASE_RATIO
+	boost_speed_modifier += blood_value * SPEED_INCREASE_RATIO
 
 	var blood_total = BASE_BLOOD_TOTAL + blood_total_modifier
 	blood = blood_total
 	blood_total_bar.rect_scale.x = blood_total / 100.0
+
+func _attack_boost():
+	velocity += Vector3.FORWARD.rotated(Vector3.UP, rotation.y) * ATTACK_VELOCITY
+
+func _attack_end():
+	animation.play("Idle", 0.5)
+	is_attacking = false
 
