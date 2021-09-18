fd0dd0e93e1729983dc8311531463dc5881ebe33
crysopraise <assignedcatgirlatbirth@protonmail.com>
2021-09-17 17:04:32 -0700

    Changed player boost
    - Changed the player boost to be more like a short dodge then an
      infinite speed boost
    - Removed some of the code cysts altering base/boost speeds
    - Added forward/back tilting of the player model
    - Added some small comments
    - Added *.swp to .gitignore

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,49 +1,66 @@
 extends KinematicBody
 
+# Movement constants
 export var VELOCITY = 5
 export var ACCELERATION = 0.015
 export var DRAG = 0.02
+export var BOOST_VELOCITY = 25
+export var BOOST_ACCELRATION = 0.15
+export var BOOST_LENGTH = 0.4
 export var ATTACK_VELOCITY = 15
+
+# Mouse/look constants
 export var MOUSE_SENSITIVITY = 0.1
-export var MAX_VERTICLE_LOOK_ANGLE = 30
+export var MAX_VERTICLE_LOOK_ANGLE = 60
 export var BASE_LOOK_ANGLE = 0
-export var STRAFE_TILT_ANGLE = 10
+
+# Model Tilt and Camera lag constants
+export var STRAFE_TILT_ANGLE = 5
 export var CAMERA_LAG = 0.01
 export var CAMERA_LAG_RATIOS = Vector3(8, 3, 4)
 
 # Blood constants
 export var BASE_BLOOD_TOTAL = 100
 export var BASE_BLOOD_REGEN = 1 # per second
-export var BOOST_BLOOD_COST = 40 # per second
+export var BOOST_BLOOD_COST = 20
 export var ATTACK_BLOOD_COST = 15
-export var BASE_BOOST_SPEED_MODIFIER = 2.5
-export var BASE_BOOST_ACCEL_MODIFIER = 5
-#export var BASE_BOOST_MAX_VEL_MODIFIER = 1.5
+# Unused atm
+export var BASE_BOOST_SPEED_MODIFIER = 4
+export var BASE_BOOST_ACCEL_MODIFIER = 7.5
+export var BASE_BOOST_MAX_VEL_MODIFIER = 1.5
 
 # Blood value constants
 export var TOTAL_INCREASE_RATIO = 10
 export var REGEN_INCREASE_RATIO = 1
 export var SPEED_INCREASE_RATIO = 0
 
+# Scenes
 onready var player_attack_scn = preload("res://scenes/player/player_attack.tscn")
 
+# Nodes
 onready var camera = $Pivot
 onready var model = $Model
 onready var blood_remaining_bar = $HealthBar/Remaining
 onready var blood_total_bar = $HealthBar/Total
 onready var animation = $Model/AnimationPlayer
 
+# Variables
 var velocity = Vector3.ZERO
+var boost_direction = Vector3.ZERO
 var blood = BASE_BLOOD_TOTAL
-var blood_total_modifier = 0
-var blood_regen_modifier = 0
-var blood_speed_modifier = 1
-var boost_speed_modifier = BASE_BOOST_SPEED_MODIFIER
-var boost_accel_modifier = BASE_BOOST_ACCEL_MODIFIER
-#var boost_vel_modifier = BASE_BOOST_MAX_VEL_MODIFIER
+var is_boosting = false
 var is_attacking = false
 var camera_offset
 var player_attack
+
+var blood_total_modifier = 0
+var blood_regen_modifier = 0
+
+# Unused atm
+var blood_speed_modifier = 1
+var boost_speed_modifier = BASE_BOOST_SPEED_MODIFIER
+var boost_accel_modifier = BASE_BOOST_ACCEL_MODIFIER
+var boost_vel_modifier = BASE_BOOST_MAX_VEL_MODIFIER
 
 func _ready():
 	# Prevent mouse from going off screen
@@ -55,6 +72,7 @@ func _ready():
 	# Should do this in the editor lol
 	$AttackTimer.wait_time = 1.5
 	$AttackTimer.connect("timeout", self, "_on_attack_timeout")
+	$BoostTimer.wait_time = BOOST_LENGTH
 
 	# Set blood bar size
 	blood_total_bar.rect_scale.x = BASE_BLOOD_TOTAL / 100.0
@@ -70,8 +88,6 @@ func _input(event):
 			camera.rotation.x,
 			deg2rad(BASE_LOOK_ANGLE - MAX_VERTICLE_LOOK_ANGLE),
 			deg2rad(BASE_LOOK_ANGLE + MAX_VERTICLE_LOOK_ANGLE))
-		# Rotate model to match camera
-		model.rotation.x = camera.rotation.x
 
 		# Rotate character left and right
 		rotation.y += -deg2rad(movement.x * MOUSE_SENSITIVITY)
@@ -79,7 +95,6 @@ func _input(event):
 func _physics_process(delta):
 	var input_direction = Vector3.ZERO
 	var attack_velocity = Vector3.ZERO
-	var is_boosting = false
 
 	# Directional inputs
 	# Use direction of camera to determine movement direction
@@ -96,37 +111,48 @@ func _physics_process(delta):
 	if Input.is_action_pressed('up'):
 		input_direction += camera.global_transform.basis.y
 	
+	# Normalize input
+	input_direction = input_direction.normalized()
+	
 	# Attack and boost
 	#if Input.is_action_pressed('attack') and !is_attacking:
 	#	blood -= ATTACK_BLOOD_COST
 	#	animation.play("Attack", 0.5)
 	#	is_attacking = true
-	if Input.is_action_pressed('boost') and blood > 0:
+	if Input.is_action_just_pressed('boost') and blood > 0 and !is_boosting:
+		blood -= BOOST_BLOOD_COST
+		boost_direction = input_direction
 		is_boosting = true
-		blood -= BOOST_BLOOD_COST * delta
+		$BoostTimer.start()
 	else:
 		var regen = (BASE_BLOOD_REGEN + blood_regen_modifier) * delta
 		blood = min(blood + regen, BASE_BLOOD_TOTAL + blood_total_modifier)
+	
+	# Debug
+	if Input.is_action_just_pressed('debug_button'):
+		blood = BASE_BLOOD_TOTAL
 
 	# Get the maximum velocity (scalar)
-	var max_velocity = VELOCITY * boost_speed_modifier \
-			if is_boosting else VELOCITY * blood_speed_modifier
+	var max_velocity = BOOST_VELOCITY if is_boosting else VELOCITY
 	
 	# Multiply directional input by velocity
-	var target_velocity = input_direction.normalized() * max_velocity
+	var target_velocity = (boost_direction if is_boosting else input_direction) \
+			* max_velocity
 	
 	if input_direction.length() > 0:
 		# Accelerate towards target velocity if there is input
-		var accelaration = ACCELERATION * boost_accel_modifier \
-				if is_boosting else ACCELERATION * blood_speed_modifier
-		velocity = velocity.linear_interpolate(target_velocity, accelaration)
+		var acceleration = BOOST_ACCELRATION if is_boosting else ACCELERATION
+		velocity = velocity.linear_interpolate(target_velocity, acceleration)
 	else:
 		# Decelerate if there is no input
-		velocity = velocity.linear_interpolate(Vector3.ZERO, DRAG)
+		velocity = velocity.linear_interpolate(Vector3.ZERO, ACCELERATION)
 
 	# Rotate model according to velocity
 	var rotated_velocity = velocity.rotated(Vector3.UP, -rotation.y)
 	model.rotation.z = -rotated_velocity.x \
+			/ VELOCITY \
+			* deg2rad(STRAFE_TILT_ANGLE)
+	model.rotation.x = camera.rotation.x + rotated_velocity.z \
 			/ VELOCITY \
 			* deg2rad(STRAFE_TILT_ANGLE)
 
@@ -160,8 +186,9 @@ func _on_entity_hit(body):
 func _increase_blood(blood_value):
 	blood_total_modifier += blood_value * TOTAL_INCREASE_RATIO
 	blood_regen_modifier += blood_value * REGEN_INCREASE_RATIO
-	boost_speed_modifier += blood_value * SPEED_INCREASE_RATIO
+	# boost_speed_modifier += blood_value * SPEED_INCREASE_RATIO
 
+	# Update blood bar total
 	var blood_total = BASE_BLOOD_TOTAL + blood_total_modifier
 	blood = blood_total
 	blood_total_bar.rect_scale.x = blood_total / 100.0
@@ -172,4 +199,8 @@ func _attack_boost():
 func _attack_end():
 	animation.play("Idle", 0.5)
 	is_attacking = false
+
+
+func _on_boost_end():
+	is_boosting = false
 
--- scripts/testworld.gd
+++ scripts/testworld.gd
@@ -8,11 +8,11 @@ func _ready():
 func _process(delta):
 	$FPS.text = str(Engine.get_frames_per_second()) + " fps"
 
-	if Input.is_action_just_pressed("debug_button"):
-		var guy = preload("res://scenes/enemies/enemy.tscn").instance()
-		guy.translation = $Player.translation
-		guy.translation.z -= 3
-		add_child(guy)
-		guy_count += 1
-		$GuyCount.text = "Guys: " + str(guy_count)
+	#if Input.is_action_just_pressed("debug_button"):
+	#	var guy = preload("res://scenes/enemies/enemy.tscn").instance()
+	#	guy.translation = $Player.translation
+	#	guy.translation.z -= 3
+	#	add_child(guy)
+	#	guy_count += 1
+	#	$GuyCount.text = "Guys: " + str(guy_count)
 
