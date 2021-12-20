105601f43576e245ddf15e6e17bc2500b1983e4c
crysopraise <assignedcatgirlatbirth@protonmail.com>
2021-12-19 19:12:13 -0800

    Blood bar animation increases speed
    - Blood bar animation increases speed in accordance to the blood level
      (enemies killed)

--- scripts/gui/player_gui.gd
+++ scripts/gui/player_gui.gd
@@ -7,6 +7,8 @@ onready var debug_velocity = $VContainer/HContainer/Debug/Velocity
 
 onready var base_blood_size = blood_bar.rect_min_size.x
 onready var base_message_size = message_text.get('custom_fonts/font').size
+onready var base_beat_frequency = blood_bar.material.get_shader_param('beat_frequency')
+onready var base_flow_rate = blood_bar.material.get_shader_param('flow_rate')
 
 func _process(delta):
 	fps_counter.text = str(Engine.get_frames_per_second()) + " fps"
@@ -16,6 +18,11 @@ func update_blood(current, total):
 	blood_bar.rect_min_size.x += base_blood_size * ((total - blood_bar.max_value) / total)
 	blood_bar.material.set_shader_param("bar_percentage", current)
 	blood_bar.max_value = total
+
+func set_blood_bar_speed(multiplier):
+	blood_bar.material.set_shader_param('beat_frequency', base_beat_frequency * multiplier)
+	blood_bar.material.set_shader_param('flow_rate', base_flow_rate * multiplier)
+	
 
 func display_message(text, font_size = 0, time = 4):
 	message_text.text = text
--- scripts/level_manager.gd
+++ scripts/level_manager.gd
@@ -5,6 +5,7 @@ var gui
 var enemy_total: float = 0
 var enemy_count: float = 0
 var current_level_type = 0
+var initial_blood_level = 0
 
 func _ready():
 	randomize()
@@ -14,7 +15,8 @@ func _unhandled_input(event):
 		if event.pressed and event.scancode == KEY_ESCAPE:
 			get_tree().quit()
 
-func on_level_load(level_type, initial_blood_level):
+func on_level_load(level_type, blood_level):
+	initial_blood_level = blood_level
 	BulletManager.reset_pools()
 	var message = ''
 	current_level_type = level_type
@@ -29,10 +31,12 @@ func on_level_load(level_type, initial_blood_level):
 	get_tree().call_group('hazard', 'set_blood_level', initial_blood_level)
 	gui = get_tree().current_scene.get_node('GUI')
 	gui.display_message(message, 60)
+	gui.set_blood_bar_speed(1 + initial_blood_level)
 
 func reset_level():
 	BulletManager.reset_bullets()
 	get_tree().reload_current_scene()
+	gui.set_blood_bar_speed(1 + initial_blood_level)
 
 func agro_enemies():
 	get_tree().call_group('enemies', 'alert_player_moved')
@@ -40,9 +44,10 @@ func agro_enemies():
 		gui.clear_message()
 
 func on_enemy_die():
+	enemy_count -= 1
 	get_tree().call_group('hazard', 'add_blood_level', 1 / enemy_total)
+	gui.set_blood_bar_speed(1.0 + (1 - (enemy_count / enemy_total)))
 	if current_level_type == Level.LevelType.PURGE:
-		enemy_count -= 1
 		var player = get_tree().current_scene.get_node('Player')
 		if enemy_count == 0 and !player.is_dead:
 			get_tree().call_group('bullets', 'die')
