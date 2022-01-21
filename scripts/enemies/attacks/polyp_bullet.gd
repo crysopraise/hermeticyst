0a911aeae17c388f165acb73ad70d71d28d95b29
crysopraise <assignedcatgirlatbirth@protonmail.com>
2022-01-20 20:36:12 -0800

    Fixed polyp attack
    - Fixed but where polyp bullet grow animation only played on first shot
    - Made polyp attack continously and shoot while bullets still exist
    - Added death time to bullets (15 seconds)

--- scripts/enemies/attacks/polyp_bullet.gd
+++ scripts/enemies/attacks/polyp_bullet.gd
@@ -10,11 +10,13 @@ onready var player = get_tree().current_scene.get_node_or_null('Player')
 func move(delta):
 	if animation_finished:
 		if is_instance_valid(player):
+			var distance = speed * delta
 			var target_rotation = global_transform.looking_at(player.global_transform.origin, Vector3.UP)
 			global_transform = global_transform.interpolate_with(target_rotation, delta * TURN_SPEED)
-			translate(Vector3.FORWARD * delta * speed)
+			translate(Vector3.FORWARD * distance)
+			update_distance_traveled(distance)
 
 func fire(params):
 	.fire(params)
+	animation_finished = false
 	$AnimationPlayer.play('grow')
-
