2ac7ba7334e8d4293ab4af4e7218ba38cbdd2fab
crysopraise <assignedcatgirlatbirth@protonmail.com>
2022-02-03 13:20:40 -0800

    Added beam attack
    - Added a beam attack that you can spend your extra life to shoot, also
      recovers 50% blood
    - Added in shoot animation and tweaked it so it plays partway, finishes
      when firing, and reverses when unaiming without firing
    - Tweaked the beam attack so that it lines up directly with the center
      of you screen, added a temp aiming reticle
    - There is a bug that fucks up you beam when the camera clips outside
      the level

--- scripts/tools/line_drawer.gd
+++ scripts/tools/line_drawer.gd
@@ -2,6 +2,11 @@ extends ImmediateGeometry
 
 var points = []
 
+func _ready():
+	var material = SpatialMaterial.new()
+	material.vertex_color_use_as_albedo  = true
+	set_material_override(material)
+
 func add_line(start: Vector3, end: Vector3):
 	points.append(start)
 	points.append(end)
@@ -10,6 +15,7 @@ func _physics_process(delta):
 	clear()
 
 	begin(Mesh.PRIMITIVE_LINES)
+	set_color(Color(1, 0, 0))
 
 	for point in points:
 		add_vertex(point)
