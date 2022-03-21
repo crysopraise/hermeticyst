59ce875f828dbfcbf280ce2e437a231f1971b3b7
Hilda Hay <hilda@hildahay.ca>
2022-03-20 18:10:18 -0700

    Added expanding and dissolving animations to orb

--- scenes/other/enemy_spawn_animation/enemy_spawn_orb.gd
+++ scenes/other/enemy_spawn_animation/enemy_spawn_orb.gd
@@ -2,12 +2,27 @@ extends Spatial
 
 var mesh;
 var mesh_size_factor;
+var timer;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	mesh_size_factor = 0;
+	timer = 0;
 	mesh = get_node("MeshInstance");
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	mesh_size_factor = 0.001;
+	mesh.scale = Vector3(mesh_size_factor, mesh_size_factor, mesh_size_factor);
+	
+func _process(delta):
+	timer += delta;
+	if mesh_size_factor < 1.0:
+		mesh_size_factor += delta * 1.0;
+		if mesh_size_factor > 1.0:
+			mesh_size_factor = 1.0;
+		print(mesh_size_factor);
+		mesh.scale = Vector3(mesh_size_factor, mesh_size_factor, mesh_size_factor);
+	else:
+		var dissolve_amount = timer - 2.0;
+		if dissolve_amount >= 0.0:
+			var material = mesh.get_surface_material(0);
+			material.set_shader_param("dissolve_amount", dissolve_amount);
+			mesh.set_surface_material(0, material);
+	
